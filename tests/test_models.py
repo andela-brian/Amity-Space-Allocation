9bfb892d6d80cb4031fd036f882c5981d0efe709
Brian Rotich <brian.rotich@andela.com>
2016-11-08 18:51:23 +0300

    Remove redundant test for default values in the Person Class and subclasses

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,57 +2,48 @@ import unittest
 
 
 class StaffClassTest(unittest.TestCase):
-    def setUp(self):
-        self.default_staff = Staff()
+    def test_staff_name(self):
+        staff_name = "John Doe"
+        staff = Staff()
 
-    def test_staff_is_subclass_of_person(self):
-        self.assertIsInstance(self.default_staff, Person,
-                              msg="Staff Class should be a subclass of Person")
+        staff.set_name(staff_name)
 
-    def test_staff_default_values(self):
-        self.assertEqual(True, self.default_staff.is_staff(),
-                         msg="is_staff should be true for Staff class")
-        self.assertEqual(False, self.default_staff.is_allocated_office(),
-                         msg="Staff should not be allocated an Office when Created")
-        self.assertIsNone(self.default_staff.get_allocated_office_space(),
-                          msg="Staff allocated Office Space should be None before an Office Space is assigned")
+        self.assertEqual(staff_name, staff.get_name(),
+                         msg="Staff name should be {0}".format(staff_name))
+
+    def test_staff_office_allocation(self):
+        staff_name = "John Doe"
+        office_space = "Hogwarts"
+
+        staff = Staff(name=staff_name, office_space=office_space)
+
+        self.assertListEqual([staff_name, office_space],
+                             [staff.get_name(), staff.get_allocated_office()],
+                             msg="Staff {0} should be assigned office space at {1}".format(staff_name, office_space))
+
+        self.assertTrue(staff.is_assigned(),
+                        msg="is_assigned function should be True when staff is allocated space")
 
 
 class FellowClassTest(unittest.TestCase):
-    def setUp(self):
-        self.fellow_default = Fellow("Brian Test")
+    def test_fellow_name(self):
+        fellow_name = "Brian Test"
+        fellow = Fellow()
 
-    def test_fellow_is_subclass_of_person(self):
-        self.assertIsInstance(Fellow(), Person,
-                              msg="Fellow class should be subclass of Person")
+        fellow.set_name(fellow_name)
 
-    def test_fellow_get_name(self):
-        self.assertEqual("Brian Test", self.fellow_default.get_name(),
+        self.assertEqual(fellow_name, fellow.get_name(),
                          msg="The Fellow name should be Brian Test")
 
-    def test_default_is_fellow(self):
-        self.assertEqual(True, self.fellow_default.is_fellow(),
-                         msg="The is_fellow function should be true for Fellow class")
-
-    def test_default_accomodation_is_false(self):
-        self.assertEqual(False, self.fellow_default.needs_accomodation(),
-                         msg="Fellow should not require accomodation when created ")
-
-    def test_default_is_allocated_living_space(self):
-        self.assertEqual(False, self.fellow_default.is_allocated_living_space(),
-                         msg="Fellow should not be allocated Living Space when created")
-
-    def test_default_is_allocated_office_space(self):
-        self.assertEqual(False, self.fellow_default.is_allocated_living_space(),
-                         msg="Fellow should not be allocated Office Space when created")
-
-    def test_default_set_allocated_office_and_living_space(self):
+    def test_allocated_office_and_living_space(self):
         office_space = "Hogwarts"
         living_space = "PHP"
-        self.fellow_default.set_allocated_office_space(office_space)
-        self.fellow_default.set_allocated_living_space(living_space)
+        fellow = Fellow()
+
+        fellow.set_allocated_office_space(office_space)
+        fellow.set_allocated_living_space(living_space)
 
         self.assertListEqual([office_space, living_space],
-                             [self.fellow_default.get_allocated_office_space(),
-                              self.fellow_default.get_allocated_living_space()],
+                             [fellow.get_allocated_office_space(),
+                              fellow.get_allocated_living_space()],
                              msg="Fellow should be assigned Hogwarts Office and PHP living space")
