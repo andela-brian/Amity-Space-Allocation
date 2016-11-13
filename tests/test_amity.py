53acdbd329013cca48fc74a716ab5620b5aee7ce
Brian Rotich <brian.rotich@andela.com>
2016-11-14 01:11:24 +0300

    added test for create_room function in Amity
    -replaced get_living_space and get_offices with get_rooms function
     that returns a dictionary with living_space and offices as keys

--- tests/test_amity.py
+++ tests/test_amity.py
@@ -17,8 +17,8 @@ class TestAmityClass(TestCase):
         self.assertListEqual(
             [0, 0],
             [
-                len(self.amity.get_offices()['available']),
-                len(self.amity.get_offices()['unavailable'])
+                len(self.amity.get_rooms()['offices']['available']),
+                len(self.amity.get_rooms()['offices']['unavailable'])
             ],
             msg="The available and unavailable offices should be empty on creating new amity object"
         )
@@ -26,29 +26,39 @@ class TestAmityClass(TestCase):
         self.assertListEqual(
             [0, 0],
             [
-                len(self.amity.get_living_spaces()['available']),
-                len(self.amity.get_living_spaces()['unavailable'])
+                len(self.amity.get_rooms()['living_spaces']['available']),
+                len(self.amity.get_rooms()['living_spaces']['unavailable'])
             ],
             msg="The available and unavailable living rooms should be empty on creating new amity object"
         )
 
     def test_create_fellow(self):
+        self.amity = Amity()
         self.amity.create_living_space("Perl")
         self.amity.create_office("Homer")
         self.amity.create_fellow(first_name="John", other_name="Dow", accomodation='Y')
         self.amity.create_fellow(first_name="Brian", other_name="Kimani")
 
         allocations = self.amity.get_allocations()
-        self.assertEqual(1, len(allocations['Perl'].get_allocations()),
+        self.assertEqual(1, len(allocations['living_spaces']['Perl'].get_allocations()),
                          msg="Only One fellow should be allocated to Perl Living Space")
-        self.assertEqual(2, len(allocations['Homer'].get_allocations()),
+        self.assertEqual(2, len(allocations['offices']['Homer'].get_allocations()),
                          msg="Only Two fellows should be allocated to Homer office space")
 
-    def test_create_room(self):
-        pass
+    def test_create_rooms(self):
+        self.amity = Amity()
+        self.amity.create_living_space("Summer")
+        self.amity.create_living_space("Winter")
+        self.amity.create_office("Lime")
+        self.amity.create_office("Blue")
 
-    def test_relocate_person(self):
-        pass
+        offices = self.amity.get_rooms()['offices']
+        living_spaces = self.amity.get_rooms()['living_spaces']
 
-    def test_get_available_rooms(self):
-        pass
+        self.assertListEqual(["Summer", "Winter"],
+                             offices,
+                             msg="Offices in Amity should be Summer and Winter")
+
+        self.assertListEqual(["Lime", "Blue"],
+                             offices,
+                             msg="Living Spaces in Amity should be Lime and Blue")
