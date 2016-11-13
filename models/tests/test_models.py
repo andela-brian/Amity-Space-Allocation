cee4ffa139425fadd44f620b9166d1cf4d0c92bd
Brian Rotich <brian.rotich@andela.com>
2016-11-13 11:40:14 +0300

    added tests for allocate Function in Living class

--- models/tests/test_models.py
+++ models/tests/test_models.py
@@ -41,12 +41,14 @@ class TestStaffClass(TestCase):
 
     def test_staff_is_subclass_of_person(self):
         self.assertIsInstance(self.staff, Person,
-                              msg="Fellow class should subclass of Person")
+                              msg="Staff class should subclass of Person")
 
     def test_create_new_staff(self):
         self.assertListEqual([self.id, self.name],
-                             [self,staff.get_id(), self.staff.get_name()],
+                             [self, self.staff.get_id(), self.staff.get_name()],
                              msg="The Staff named {0} should have id {1}".format(self.name, self.id))
+        self.assertRaises(ValueError, Staff, id=self.id, name=None,
+                          msg="cannot create Staff with name as None type")
 
 
 class TestLivingRoomClass(TestCase):
@@ -63,6 +65,25 @@ class TestLivingRoomClass(TestCase):
         self.assertEqual(4, self.living_space.get_capacity(),
                          msg="Living Space should have a total of 4 spaces")
 
+    def test_allocating_living_space(self):
+        self.assertTrue(self.living_space.is_available(),
+                        msg="Living Space should be available before allocation of spaces")
+        staff = Staff(id="STF01", name="John Doe")
+
+        self.assertRaises(TypeError, self.living_space.allocate_space(staff),
+                          msg="A staff member cannot be allocated Living Space")
+
+        # create test data for fellows
+        fellows = [Fellow(id=fellow_id, name="Fellow Name " + str(fellow_id)) for fellow_id in xrange(1, 6)]
+        for fellow_id in xrange(4):
+            self.living_space.allocate_space(fellows.pop())
+
+        self.assertListEqual(fellows, self.living_space.get_allocations(),
+                             msg="Living Space should contain four fellows after allocation")
+
+        self.assertRaises(ValueError, self.living_space.allocate_space, fellows.pop(),
+                          msg="Living space cannot be allocated to more than 4 fellows")
+
 
 class TestOfficeClass(TestCase):
     def setUp(self):
@@ -73,6 +94,6 @@ class TestOfficeClass(TestCase):
         self.assertIsInstance(self.office_space, Room,
                               msg="LivingRoom class should be a subclass of Room")
 
-    def test_office_romm_capacity(self):
+    def test_office_room_capacity(self):
         self.assertEqual(6, self.office_space.get_capacity(),
                          msg="Office should have a total of 6 spaces ")
