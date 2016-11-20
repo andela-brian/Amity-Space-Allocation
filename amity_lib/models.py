c5a21deacb28537861ca3009ccd27e7c4cdc5127
Brian Rotich <brian.rotich@andela.com>
2016-11-20 21:37:34 +0300

    add test for LivingSpace, Office class

--- amity_lib/models.py
+++ amity_lib/models.py
@@ -1,4 +1,5 @@
 class Person(object):
+
     name = None
     office = None
 
--- tests/test_models.py
+++ tests/test_models.py
@@ -2,11 +2,10 @@ import unittest
 
 import mock
 
-from amity_lib.models import Staff, Fellow, Office
+from amity_lib.models import Staff, Fellow, Office, LivingSpace
 
 
 class TestStaffClass(unittest.TestCase):
-
     def setUp(self):
         self.staff = Staff("John Maina")
 
@@ -32,10 +31,33 @@ class TestOfficeClass(unittest.TestCase):
         self.assertEqual(6, self.office.get_capacity(),
                          msg="Office capacity should be 6")
 
-    @mock.patch('amity_lib.models.Office.persons_id', ['FL001', 'ST001', 'FL003'])
+    @mock.patch('amity_lib.models.Office.persons_id', ['FL001', 'ST001', 'FL003', 'ST004', 'ST002'])
     def test_allocate_space(self):
         self.assertTrue(self.office.is_available(), msg="Office should be available when not full")
+        self.assertRaises(ValueError, self.office.allocate_space('S001'),
+                          msg="The person_id prefix should be 'ST' or 'FL'")
         self.assertEqual('ST002 allocated Lilac', self.office.allocate_space('ST002'),
                          msg="ST002 should be allocated space in Lilac")
         self.assertRaises(ValueError, self.office.allocate_space('FL003'),
                           msg="cannot allocate person to a full room")
+
+
+class TestLivingSpaceClass(unittest.TestCase):
+    def setUp(self):
+        self.living_space = LivingSpace("Shell")
+
+    def test_get_capacity(self):
+        self.assertEqual(4, self.living_space.get_capacity(),
+                         msg="Living Space capacity should be 4")
+
+    @mock.patch('amity_lib.models.LivingSpace.persons_id', ['FL001', 'FL002', 'FL003'])
+    def test_allocate_space(self):
+        self.assertTrue(self.living_space.is_available(), msg="Living Space should be available when not full")
+        self.assertRaises(ValueError, self.living_space.allocate_space('S001'),
+                          msg="The person_id prefix should be 'ST' or 'FL'")
+        self.assertRaises(ValueError, self.living_space.allocate_space('ST001'),
+                          msg="Cannot allocate Staff to Living Space")
+        self.assertEqual('FL004 allocated Lilac', self.living_space.allocate_space('FL004'),
+                         msg="FL004 should be allocated space in Shell")
+        self.assertRaises(ValueError, self.living_space.allocate_space('FL005'),
+                          msg="Cannot allocate fellow to a full Living Space")
